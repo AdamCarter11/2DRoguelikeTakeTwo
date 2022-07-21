8775f05325bc006c4c8ab271f7c380ac4d3598d9
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-07-21 16:28:25 -0700

    Test with flat speed

--- RoguelikeTakeTwo/Assets/Scripts/ButtonUpgrades.cs
+++ RoguelikeTakeTwo/Assets/Scripts/ButtonUpgrades.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class ButtonUpgrades : MonoBehaviour
 {
+    [SerializeField] GameObject panelPopUp;
     Button bt;
     private void Start() {
         bt = this.GetComponent<Button>();
@@ -15,5 +16,12 @@ public class ButtonUpgrades : MonoBehaviour
     public void ApplyUpgrade(){
         string abilityName = this.GetComponent<Image>().sprite.name;
         print(abilityName);
+
+        if(abilityName == "Circle"){
+            GameManager.Instance.flatSpeedModifier+=5;
+        }
+
+        panelPopUp.SetActive(false);
+        Time.timeScale = 1;
     } 
 }
