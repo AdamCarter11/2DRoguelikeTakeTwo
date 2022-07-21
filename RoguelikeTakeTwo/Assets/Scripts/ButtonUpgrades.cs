84e03e75854918d12d928c86152417d149f474d3
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-07-21 16:20:42 -0700

    buttons

--- RoguelikeTakeTwo/Assets/Scripts/ButtonUpgrades.cs
+++ RoguelikeTakeTwo/Assets/Scripts/ButtonUpgrades.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class ButtonUpgrades : MonoBehaviour
 {
-    
+    Button bt;
+    private void Start() {
+        bt = this.GetComponent<Button>();
+        bt.onClick.AddListener(ApplyUpgrade);
+    }
+
+    public void ApplyUpgrade(){
+        string abilityName = this.GetComponent<Image>().sprite.name;
+        print(abilityName);
+    } 
 }
